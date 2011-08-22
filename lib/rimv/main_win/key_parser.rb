c9e023ddde5fb5571533284033afa347cf1e9367
Katsuhiko Nishimra <ktns.87@gmail.com>
2011-08-22 11:18:38 +0900

    KeyParser: Key value constant workaround

--- lib/rimv/main_win/key_parser.rb
+++ lib/rimv/main_win/key_parser.rb
@@ -3,6 +3,13 @@ class Rimv::MainWin
 		include Rimv
 		include Gdk::Keyval
 
+		def self.const_missing id
+			if constants.include?(oid = id.to_s.sub('GDK_KEY_', 'GDK_'))
+				const_set(id, const_get(oid))
+			else
+				super
+			end
+		end
 		def initialize
 			@stack = []
 		end
