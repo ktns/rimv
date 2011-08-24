84c5f05f6364da005699f6a249a7f1e8c0fd13ba
Katsuhiko Nishimra <ktns.87@gmail.com>
2011-08-25 04:18:23 +0900

    Moved database type independent codes

--- lib/rimv/db/adaptor.rb
+++ lib/rimv/db/adaptor.rb
@@ -1,7 +1,46 @@
 module Rimv
 	module DB
 		module Adaptor
-
+			include Rimv
+			def addfile path, base=false
+				@base = path.sub(/\/*$/,'') if base
+				begin
+					if File.directory?(path)
+						Dir.foreach(path) do |file|
+							next if %w<. ..>.include?(file)
+							verbose(1).puts "adding directory `#{path}/#{file}'"
+							addfile("#{path}/#{file}")
+						end
+					elsif File.file?(path)
+						img = Gtk::Image.new(path)
+						if img.pixbuf || img.pixbuf_animation
+							File.open(path) do |file|
+								verbose(1).puts "adding file `#{path}'"
+								hash = addimage(path,file.read)
+								if @base
+									verbose(3).puts "tag base = #{@base}"
+									tag = File.dirname(path.sub(/^#{Regexp.escape(@base)}\/*/,''))
+									unless tag == '.'
+										addtag hash, tag
+									end
+								elsif @@tag
+									verbose(3).puts "tagging `#{path}'(#{hash}) as `#{@@tag}'"
+									addtag hash, @@tag
+								end
+							end
+						else
+							$stderr.puts "`#{path}' is not a image supported by gtk!"
+							verbose(2).puts "image            = #{img.inspect}"
+							verbose(2).puts "pixbuf           = #{img.pixbuf.inspect}"
+							verbose(2).puts "pixbuf_animation = #{img.pixbuf_animation.inspect}"
+						end
+					else
+						$stderr.puts "file `#{path}' does not exist!"
+					end
+				ensure
+					@base = nil if base
+				end
+			end
 		end
 	end
 end
--- lib/rimv/db/adaptor/sqlite3.rb
+++ lib/rimv/db/adaptor/sqlite3.rb
@@ -3,7 +3,6 @@ module Rimv
 		module Adaptor
 			class SQLite3
 
-				include Rimv
 				include Adaptor
 
 				require 'sqlite3'
@@ -51,46 +50,6 @@ SELECT :hash, :name
 WHERE NOT EXISTS (SELECT 1 FROM name WHERE hash=:hash AND name=:name);
 						SQL
 					end and hash
-				end
-
-				def addfile path, base=false
-					@base = path.sub(/\/*$/,'') if base
-					begin
-						if File.directory?(path)
-							Dir.foreach(path) do |file|
-								next if %w<. ..>.include?(file)
-								verbose(1).puts "adding directory `#{path}/#{file}'"
-								addfile("#{path}/#{file}")
-							end
-						elsif File.file?(path)
-							img = Gtk::Image.new(path)
-							if img.pixbuf || img.pixbuf_animation
-								File.open(path) do |file|
-									verbose(1).puts "adding file `#{path}'"
-									hash = addimage(path,file.read)
-									if @base
-										verbose(3).puts "tag base = #{@base}"
-										tag = File.dirname(path.sub(/^#{Regexp.escape(@base)}\/*/,''))
-										unless tag == '.'
-											addtag hash, tag
-										end
-									elsif @@tag
-										verbose(3).puts "tagging `#{path}'(#{hash}) as `#{@@tag}'"
-										addtag hash, @@tag
-									end
-								end
-							else
-								$stderr.puts "`#{path}' is not a image supported by gtk!"
-								verbose(2).puts "image            = #{img.inspect}"
-								verbose(2).puts "pixbuf           = #{img.pixbuf.inspect}"
-								verbose(2).puts "pixbuf_animation = #{img.pixbuf_animation.inspect}"
-							end
-						else
-							$stderr.puts "file `#{path}' does not exist!"
-						end
-					ensure
-						@base = nil if base
-					end
 				end
 
 				def addtag hash, tag
