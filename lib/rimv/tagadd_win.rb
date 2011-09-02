5b6ac3eac93d6d697287115c9f963b7710ed0504
Katsuhiko Nishimra <ktns.87@gmail.com>
2011-09-03 04:30:05 +0900

    TagaddWin: textbox size

--- lib/rimv/tagadd_win.rb
+++ lib/rimv/tagadd_win.rb
@@ -1,17 +1,21 @@
 require 'gtk2'
+require 'rimv/db/adaptor'
 
 class Rimv::TagaddWin < Gtk::Window
-	def initialize
+	def initialize adaptor
 		super 'Add a tag'
+		raise TypeError unless adaptor.kind_of?(Rimv::DB::Adaptor)
 		add @vbox = Gtk::VBox.new
 		@vbox.add @entry = Gtk::Entry.new
 		@vbox.add @hbox  = Gtk::HBox.new
 		@hbox.add @ok = Gtk::Button.new('OK')
 		@hbox.add @cancel = Gtk::Button.new('Cancel')
+		@entry.width_chars = adaptor.tags_max_length + 3
 		@vbox.show_all
 		@ok.signal_connect('clicked',&method(:ok))
 		@cancel.signal_connect('clicked',&method(:cancel))
 		#@ok.grab_default
+		set_resizable false
 	end
 
 	def ok *args
