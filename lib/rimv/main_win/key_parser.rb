be344e16c79a1e20cf80f526c1ed4c0180cae611
Katsuhiko Nishimra <ktns.87@gmail.com>
2011-09-04 00:15:17 +0900

    Purged GDK Keyval workaround into a file

--- lib/rimv/main_win/key_parser.rb
+++ lib/rimv/main_win/key_parser.rb
@@ -1,15 +1,10 @@
+require 'rimv/keyval'
+
 class Rimv::MainWin
 	class KeyParser
 		include Rimv
 		include Gdk::Keyval
 
-		def self.const_missing id
-			if constants.include?(oid = id.to_s.sub('GDK_KEY_', 'GDK_'))
-				const_set(id, const_get(oid))
-			else
-				super
-			end
-		end
 		def initialize
 			@stack = []
 		end
