ee04cc8d43eb2b27c7dab9cfc4c4f6b167edf089
Katsuhiko Nishimra <ktns.87@gmail.com>
2011-09-06 21:10:25 +0900

    Adaptor#addfile now returns file hash

--- lib/rimv/db/adaptor.rb
+++ lib/rimv/db/adaptor.rb
@@ -6,11 +6,11 @@ module Rimv
 				@base = path.sub(/\/*$/,'') if base
 				begin
 					if File.directory?(path)
-						Dir.foreach(path) do |file|
+						Enumerable::Enumerator.new(Dir, :foreach, path).collect do |file|
 							next if %w<. ..>.include?(file)
 							verbose(1).puts "adding directory `#{path}/#{file}'"
 							addfile("#{path}/#{file}")
-						end
+						end.compact.flatten
 					elsif File.file?(path)
 						img = Gtk::Image.new(path)
 						if img.pixbuf || img.pixbuf_animation
@@ -27,6 +27,7 @@ module Rimv
 									verbose(3).puts "tagging `#{path}'(#{hash}) as `#{@@tag}'"
 									addtag hash, @@tag
 								end
+								return hash
 							end
 						else
 							$stderr.puts "`#{path}' is not a image supported by gtk!"
