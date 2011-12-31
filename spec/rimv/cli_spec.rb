88fd2e4cf2b2f563ba6722c7d3629c77a7cc9fd9
Katsuhiko Nishimra <ktns.87@gmail.com>
2011-12-31 13:42:29 +0900

    Reset Rimv.mode after testing CLI.parse

--- spec/rimv/cli_spec.rb
+++ spec/rimv/cli_spec.rb
@@ -2,6 +2,10 @@ require File.expand_path(File.join([File.dirname(__FILE__), %w<..>*1, 'spec_help
 
 describe Rimv::CLI do
 	describe '.parse' do
+		after :each do
+			Rimv.mode = nil
+		end
+
 		describe '-a' do
 			it 'should invoke add mode' do
 				Rimv::CLI.parse %w<-a>
