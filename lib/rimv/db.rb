1e09ef5c69976c04903e4b60bc360f9a4160f719
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-01-01 14:27:05 +0900

    Documentations for the module DB

--- lib/rimv/db.rb
+++ lib/rimv/db.rb
@@ -1,19 +1,24 @@
 require 'rimv/db/adaptor.rb'
 
 module Rimv
+	#Namespace for database related functions
 	module DB
 		@@adaptor=Adaptor::SQLite3
 
+		# Open database with the default database adaptor
 		def self.open db_file=nil, &block
 			@@adaptor.open db_file, &block
 		end
 
+		# generate MD5 digest of an image for an access key
 		def self.digest img
 			Digest::MD5.digest(img).unpack('h*').first
 		end
 
+		# Character set available in a tag string
 		TAG_CHARS='[\w.+\-_]'
 
+		# Check if the string is accectable as a tag
 		def self.acceptable_tag? tag
 			/\A#{TAG_CHARS}+(\/#{TAG_CHARS}+)*\Z/ =~ tag
 		end
