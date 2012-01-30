fbb05390d41fc377e57f4569c043d415848687de
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-01-30 16:09:49 +0900

    ruby 1.8.7 workaround
    use const_get? of parent module

--- lib/rimv/keyval.rb
+++ lib/rimv/keyval.rb
@@ -4,9 +4,10 @@ module Rimv
 
 		module ConstMissing
 			def const_missing id
-				if const_defined?(oid = id.to_s.sub('GDK_KEY_', 'GDK_'))
+				if Gdk::Keyval.const_defined?(oid = id.to_s.sub('GDK_KEY_', 'GDK_'))
 					const_set(id, const_get(oid))
 				else
+					puts oid
 					super
 				end
 			end
