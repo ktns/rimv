62e75be086af115302f31936d4ff33f1e034c607
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-02-03 02:24:37 +0900

    Document class  Rimv::DB::TagTree::Node

--- lib/rimv/db/tagtree/node.rb
+++ lib/rimv/db/tagtree/node.rb
@@ -1,8 +1,17 @@
 class Rimv::DB::TagTree
+	# Node of the bi-directional TagTree structure
 	class Node
 		include Rimv
-		attr_reader :parent, :tag, :children, :hashes
+		# Parent node
+		attr_reader :parent
+		# Tag string of the node
+		attr_reader :tag
+		# Child nodes (not leaf) of the node
+		attr_reader :children
+		# Child leaf nodes of the node
+		attr_reader :hashes
 
+		# Compares nodes by tag strings
 		def <=> other
 			raise TypeError unless other.kind_of?(self.class)
 			raise ArgumentError,
@@ -11,6 +20,7 @@ class Rimv::DB::TagTree
 			tag <=> other.tag
 		end
 
+		# Returns the TagTree object that contains the node
 		def tree
 			if @parent.instance_of?(Rimv::DB::TagTree)
 				@parent
@@ -19,6 +29,7 @@ class Rimv::DB::TagTree
 			end
 		end
 
+		# Array of nodes between the root and self
 		def path
 			path = []
 			node = self
@@ -29,10 +40,12 @@ class Rimv::DB::TagTree
 			return path
 		end
 
+		# Array of tag strings tagged the nodes between the root and self
 		def tags
 			path.collect{|n| n.tag}.compact
 		end
 
+		# Returns a string to distinguish the node
 		def to_s
 			path.collect do |node|
 				if node.parent.instance_of?(Rimv::DB::TagTree)
@@ -43,10 +56,12 @@ class Rimv::DB::TagTree
 			end.join('->')
 		end
 
+		# Returns a string to distinguish the object
 		def inspect
 			"#<#{self.class.name};#{to_s}>"
 		end
 
+		# Creates a child node of the specified parent with the specified tag
 		def initialize parent, tag
 			verbose(4).puts do
 				'Initializing new TagTree Node; ' +
@@ -63,6 +78,7 @@ class Rimv::DB::TagTree
 			@hashes       = []
 		end
 
+		# Checks consistency as a node of TagTree
 		def consistent? depth = 0
 			verbose(1).puts {"Consistency Check for tag #{@tag}, depth #{depth}"}
 			@children.each do |c|
@@ -75,6 +91,9 @@ class Rimv::DB::TagTree
 			true
 		end
 
+		# Adds Leaf nodes with tags under the node recursively and creates its parent Nodes if not exist.
+		# Creates Leaf nodes with all possible path order.
+		# Tag strings with '/' are splitted, but not their order is conserved.
 		def add hash, tags
 			raise TypeError.new('Expected Enumerable but %s for tags!' % tags.class) unless tags.kind_of?(Enumerable)
 			verbose(4).puts do
@@ -101,6 +120,7 @@ class Rimv::DB::TagTree
 			end
 		end
 
+		# Returns the first Leaf node under self or under the next Node
 		def first
 			raise ScriptError, "#{self.inspect}#\@hashes was nil!" if @hashes.nil?
 			raise ScriptError, "#{self.inspect}#\@children was nil!" if @children.nil?
@@ -111,6 +131,7 @@ class Rimv::DB::TagTree
 			raise "#{inspect}.first returned nil"
 		end
 
+		# Returns the next Leaf node of the specified Leaf node
 		def next_hash_of hash
 			@hashes[@hashes.index(hash)+1] or
 			if @children.empty?
@@ -124,6 +145,7 @@ class Rimv::DB::TagTree
 			end
 		end
 
+		# Returns the next Node of the specified Node
 		def next_node_of node
 			@children[@children.index(node)+1] or
 			unless @parent.instance_of?(Rimv::DB::TagTree)
@@ -133,10 +155,12 @@ class Rimv::DB::TagTree
 			end
 		end
 
+		# Returns the last child Leaf node
 		def last_hash
 			@hashes.last
 		end
 
+		# Returns the last child Node, if exists, or self
 		def last_node
 			if @children.empty?
 				self
@@ -145,6 +169,7 @@ class Rimv::DB::TagTree
 			end
 		end
 
+		# Returns the previous Leaf Node of the specified Leaf node
 		def prev_hash_of hash
 			if (index = @hashes.index(hash)-1) >= 0
 				@hashes[index]
@@ -160,6 +185,7 @@ class Rimv::DB::TagTree
 			end
 		end
 
+		# Returns the previous Node of the specified Node
 		def prev_node_of node
 			if ( index = @children.index(node)-1 ) >= 0
 				@children[index].last_node
@@ -168,6 +194,7 @@ class Rimv::DB::TagTree
 			end
 		end
 
+		# Enumerates all Leaf nodes under self
 		def each_leaves &block
 			raise ArgumentError, 'each_leaves called without block!' unless block.kind_of?(Proc)
 			@hashes.each &block
@@ -176,10 +203,12 @@ class Rimv::DB::TagTree
 			end
 		end
 
+		# Returns enumerator with each_leaves
 		def leaves
 			Enumerator.new(self, :each_leaves)
 		end
 
+		# Enumerates all Nodes under self including self
 		def each_nodes &block
 			raise ArgumentError, 'each_nodes called without block!' unless block.kind_of?(Proc)
 			yield self
@@ -188,10 +217,12 @@ class Rimv::DB::TagTree
 			end
 		end
 
+		# Returns enumerator with each_nodes
 		def nodes
 			Enumerator.new(self, :each_nodes)
 		end
 
+		# Returns Nodes which have tags shuffled into all possible order as path
 		def shuffle tags
 			if tags.empty?
 				self
@@ -203,6 +234,7 @@ class Rimv::DB::TagTree
 			end
 		end
 
+		# Returns the child Node with specified tag
 		def [] tag, *tags
 			unless tags.empty?
 				self[tag][*tags]
@@ -213,6 +245,7 @@ class Rimv::DB::TagTree
 			end
 		end
 
+		# Tells whether the Node have any child Node
 		def has_child? tag
 			@children.any? do |child|
 				child.tag == tag
