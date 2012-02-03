1aead8ccdadb89ab2903f155ca7ff5e87d679511
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-02-03 10:35:35 +0900

    Document class Rimv::MainWin::TagPopup

--- lib/rimv/main_win/tagpopup.rb
+++ lib/rimv/main_win/tagpopup.rb
@@ -1,10 +1,13 @@
 module Rimv
 	class MainWin
+		# This class represents a popup window to indicate tags of currently displayed image
 		class TagPopup < Gtk::Window
+			# Tells whether window is popped up
 			def on?
 				@on
 			end
 
+			# Toggles whether window is popped up
 			def toggle
 				@on = ! @on
 				if @on
@@ -14,6 +17,7 @@ module Rimv
 				end
 			end
 
+			# Creates new instance under MainWin
 			def initialize main_win
 				@on       = false
 				@main_win = main_win
@@ -28,6 +32,7 @@ module Rimv
 				end
 			end
 
+			# Display tags of the Leaf
 			def display leaf
 				unless leaf.instance_of?(Rimv::DB::TagTree::Leaf)
 					raise TypeError, 'expected Rimv::DB::TagTree::Leaf, but %s' %leaf.class
@@ -42,6 +47,7 @@ module Rimv
 				show_all if @on
 			end
 
+			# Move window to right bottom of the MainWin
 			def move
 				super(*(Size[@main_win.position]+Size[@main_win]-Size[size]))
 			end
