5ce33d030660e40a3817727b024d09de08a12f19
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-10-08 05:53:13 +0900

    Spec cosmetics

--- spec/rimv/db/adaptor_spec.rb
+++ spec/rimv/db/adaptor_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.join([File.dirname(__FILE__), %w<..>*2, 'spec_helper.rb']))
 
 describe Rimv::DB::Adaptor do
-	describe 'addfile' do
+	describe '#addfile' do
 		before :each do
 			@file = File.join(asset_path, 'logo.png')
 			@adaptor=mock('adaptor')
