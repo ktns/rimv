682aa09bc9a97be3f3bd8145236599c05680cd64
Katsuhiko Nishimra <ktns.87@gmail.com>
2014-12-12 05:59:08 +0900

    Rewrite specs about CLI

--- spec/rimv/cli_spec.rb
+++ spec/rimv/cli_spec.rb
@@ -2,39 +2,31 @@ require File.expand_path(File.join([File.dirname(__FILE__), %w<..>*1, 'spec_help
 
 describe Rimv::CLI do
 	describe '.parse' do
-		after :each do
-			Rimv.mode = nil
-		end
-
 		describe '-a' do
 			it 'should invoke add mode' do
+				expect(Rimv::Application).to receive(:new).with('add', *[anything]*5)
 				Rimv::CLI.parse %w<-a>
-				include ::Rimv
-				Rimv.mode.should == 'add'
 			end
 		end
 
 		describe '--add' do
 			it 'should invoke add mode' do
+				expect(Rimv::Application).to receive(:new).with('add', *[anything]*5)
 				Rimv::CLI.parse %w<--add>
-				include ::Rimv
-				Rimv.mode.should == 'add'
 			end
 		end
 
 		describe '-v' do
 			it 'should invoke view mode' do
+				expect(Rimv::Application).to receive(:new).with('view', *[anything]*5)
 				Rimv::CLI.parse %w<-v>
-				include ::Rimv
-				Rimv.mode.should == 'view'
 			end
 		end
 
 		describe '--view' do
 			it 'should invoke view mode' do
+				expect(Rimv::Application).to receive(:new).with('view', *[anything]*5)
 				Rimv::CLI.parse %w<--view>
-				include ::Rimv
-				Rimv.mode.should == 'view'
 			end
 		end
 	end
